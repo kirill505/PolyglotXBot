2f3bfa455343b36b151d2d9aad3a608b1f917336
Kirill <kl.bazarov@gmail.com>
2023-01-22 23:22:34 +0300

    feature: added constant messages /start and /help

--- polyglotxbot/bot.py
+++ polyglotxbot/bot.py
@@ -1,5 +1,22 @@
-import os
+import asyncio
+import config
+import message_texts
+from telebot.async_telebot import AsyncTeleBot
 
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-print("hi")
+bot = AsyncTeleBot(config.token)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    await bot.reply_to(message, message_texts.GREETINGS)
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
+
+
+asyncio.run(bot.polling())
